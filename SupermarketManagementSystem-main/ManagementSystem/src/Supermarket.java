c7c0921ee7369fbe91b29921ac41778e8c7140b3
Sanskargh7 <sanskargh5222@gmail.com>
2023-07-03 19:49:05 +0530

    first commit

---
+++ SupermarketManagementSystem-main/ManagementSystem/src/Supermarket.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarket;
+
+/**
+ *
+ * @author kanha
+ */
+public class Supermarket {
+
+    public static void main(String[] args) {
+        
+    }
+}
